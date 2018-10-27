684f5b60e9f6506da0a2572aaf5633189b5bab78
Vitaly <79vitaly79@gmail.com>
2018-10-27 08:40:46 +0300

    Added ui

---
+++ GRIPCalculator/view.h
@@ -0,0 +1,22 @@
+#ifndef VIEW_H
+#define VIEW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class View;
+}
+
+class View : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit View(QWidget *parent = nullptr);
+    ~View();
+
+private:
+    Ui::View *ui;
+};
+
+#endif // VIEW_H
