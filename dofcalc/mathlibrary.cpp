3dad685fd0891b993bd8645457acb0c4854b0ba9
Betchika99 <dobrl2011@mail.ru>
2018-11-11 16:09:31 +0300

    Added realization of main defs

--- dofcalc/mathlibrary.cpp
+++ dofcalc/mathlibrary.cpp
@@ -1,11 +1,46 @@
 #include "mathlibrary.h"
 
-MathLibrary::MathLibrary(){}
-MathLibrary::~MathLibrary(){}
-double MathLibrary::FindCrop(PropertyList *values) { return 0; }
-double MathLibrary::FindHyperFocal(PropertyList *values) { return 1.0; }
-double MathLibrary::FindNearestPointOfSharpness(PropertyList *values) { return 1.0; }
-double MathLibrary::FindFarestPointOfSharpness(PropertyList *values) { return 1.0; }
-double MathLibrary::FindGRIP(PropertyList *values) { return 0; }
+MathLibrary::MathLibrary() {}
+
+MathLibrary::~MathLibrary() {}
+
+double MathLibrary::FindCrop(PropertyList *values) {
+    double crop = values->getCrop();
+    double c = 0.03/crop;
+    return c;
+}
+
+double MathLibrary::FindHyperFocal(PropertyList *values) {
+    double f = values->getFocalLenght();
+    double c = this->FindCrop(values);
+    double d = values->getDiaphragm();
+    double h = f*f/(c*d);
+    return h;
+}
+
+double MathLibrary::FindNearestPointOfSharpness(PropertyList *values) {
+    double s0 = values->getDistanceModel();
+    double f = values->getFocalLenght();
+    double d = values->getDiaphragm();
+    double c = this->FindCrop(values);
+    double dN = (s0 * f * f)/(f * f + (f/d * c) * (s0 - f));
+    return dN;
+}
+
+double MathLibrary::FindFarestPointOfSharpness(PropertyList *values) {
+    double s0 = values->getDistanceModel();
+    double f = values->getFocalLenght();
+    double d = values->getDiaphragm();
+    double c = this->FindCrop(values);
+    double dF = (s0 * f * f)/(f * f - (f/d * c) * (s0 - f));
+    return dF;
+}
+
+double MathLibrary::FindGRIP(PropertyList *values) {
+    double dF = this->FindFarestPointOfSharpness(values);
+    double dN = this->FindNearestPointOfSharpness(values);
+    return (dF - dN);
+}
+
 void MathLibrary::Scale(PropertyList *values, ImageHandler* image) {}
 void MathLibrary::Blur(PropertyList *values, ImageHandler* image) {}
