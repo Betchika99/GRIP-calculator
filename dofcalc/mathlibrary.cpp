59d59f99cc061aac5177b63f8e86ea066b7bf2da
Betchika99 <dobrl2011@mail.ru>
2018-11-12 17:23:02 +0300

    Added template and CheckerNull def in Math

--- dofcalc/mathlibrary.cpp
+++ dofcalc/mathlibrary.cpp
@@ -1,46 +1,79 @@
 #include "mathlibrary.h"
+const int ERROR = -1;
 
-MathLibrary::MathLibrary() {}
+template<class T>
+MathLibrary<T>::MathLibrary() {}
 
-MathLibrary::~MathLibrary() {}
+template<class T>
+MathLibrary<T>::~MathLibrary() {}
 
-double MathLibrary::FindCrop(PropertyList *values) {
-    double crop = values->getCrop();
-    double c = 0.03/crop;
-    return c;
+template<class T>
+bool MathLibrary<T>::CheckerNull(const T &object) {
+    if (object == nullptr)
+            return false;
+    return true;
 }
 
-double MathLibrary::FindHyperFocal(PropertyList *values) {
-    double f = values->getFocalLenght();
-    double c = this->FindCrop(values);
-    double d = values->getDiaphragm();
-    double h = f*f/(c*d);
-    return h;
+template<class T>
+double MathLibrary<T>::FindCrop(PropertyList *values) {
+    if (this->CheckerNull()) {
+        double crop = values->getCrop();
+        double c = 0.03/crop;
+        return c;
+    }
+    return ERROR;
 }
 
-double MathLibrary::FindNearestPointOfSharpness(PropertyList *values) {
-    double s0 = values->getDistanceModel();
-    double f = values->getFocalLenght();
-    double d = values->getDiaphragm();
-    double c = this->FindCrop(values);
-    double dN = (s0 * f * f)/(f * f + (f/d * c) * (s0 - f));
-    return dN;
+template<class T>
+double MathLibrary<T>::FindHyperFocal(PropertyList *values) {
+    if (this->CheckerNull()) {
+        double f = values->getFocalLenght();
+        double c = this->FindCrop(values);
+        double d = values->getDiaphragm();
+        double h = f*f/(c*d);
+        return h;
+    }
+    return ERROR;
 }
 
-double MathLibrary::FindFarestPointOfSharpness(PropertyList *values) {
-    double s0 = values->getDistanceModel();
-    double f = values->getFocalLenght();
-    double d = values->getDiaphragm();
-    double c = this->FindCrop(values);
-    double dF = (s0 * f * f)/(f * f - (f/d * c) * (s0 - f));
-    return dF;
+template<class T>
+double MathLibrary<T>::FindNearestPointOfSharpness(PropertyList *values) {
+    if (this->CheckerNull()) {
+        double s0 = values->getDistanceModel();
+        double f = values->getFocalLenght();
+        double d = values->getDiaphragm();
+        double c = this->FindCrop(values);
+        double dN = (s0 * f * f)/(f * f + (f/d * c) * (s0 - f));
+        return dN;
+    }
+    return ERROR;
 }
 
-double MathLibrary::FindGRIP(PropertyList *values) {
-    double dF = this->FindFarestPointOfSharpness(values);
-    double dN = this->FindNearestPointOfSharpness(values);
-    return (dF - dN);
+template<class T>
+double MathLibrary<T>::FindFarestPointOfSharpness(PropertyList *values) {
+    if (this->CheckerNull()) {
+        double s0 = values->getDistanceModel();
+        double f = values->getFocalLenght();
+        double d = values->getDiaphragm();
+        double c = this->FindCrop(values);
+        double dF = (s0 * f * f)/(f * f - (f/d * c) * (s0 - f));
+        return dF;
+    }
+    return ERROR;
 }
 
-void MathLibrary::Scale(PropertyList *values, ImageHandler* image) {}
-void MathLibrary::Blur(PropertyList *values, ImageHandler* image) {}
+template<class T>
+double MathLibrary<T>::FindGRIP(PropertyList *values) {
+    if (this->CheckerNull()) {
+        double dF = this->FindFarestPointOfSharpness(values);
+        double dN = this->FindNearestPointOfSharpness(values);
+        return (dF - dN);
+    }
+    return ERROR;
+}
+
+template<class T>
+void MathLibrary<T>::Scale(PropertyList *values, ImageHandler* image) {}
+
+template<class T>
+void MathLibrary<T>::Blur(PropertyList *values, ImageHandler* image) {}
