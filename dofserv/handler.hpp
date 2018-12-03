50200bbcbc24735ebbd653ce1d56338cc481f64b
Vitaly <79vitaly79@gmail.com>
2018-12-03 12:14:35 +0300

    Changed way to make dofserv

--- dofserv/handler.hpp
+++ dofserv/handler.hpp
@@ -1,10 +1,11 @@
+
 #ifndef HANDLER_H
 #define HANDLER_H
 
 #include <event.h>
 #include <evhttp.h>
 #include <jansson.h>
-
+#include <string>
 
 void unspecified_URL(struct evhttp_request *request, void *arg);
 
